4a05bec7ddf6461250f08db167e811384349a80b
Jisang Yun <journeyofbabo@gmail.com>
2025-09-10 08:29:52 +0900

    basic way to solve prime number

--- p1_9/src/p3.rs
+++ p1_9/src/p3.rs
@@ -5,8 +5,25 @@ What is the largest prime factor of the number 600851475143?
 */
 
 #[cfg(test)]
-fn base() {
-    println!("base");
+fn find_answer(mut n: u64) -> u64 {
+    let mut p = 2; // 2 smallest prime number
+    loop {
+        let (q, r) = (n / p, n % p); // use tuple
+
+        if q == 1 {
+            break;
+        }
+
+        if r == 0 {
+            n = q;
+        }
+        // if else
+        else {
+            p += 1;
+        }
+    }
+
+    n
 }
 
 #[cfg(test)]
@@ -16,6 +33,11 @@ mod tests {
     #[test]
     fn test() {
         println!("test_template");
-        base();
+        assert_eq!(5, find_answer(10));
+        assert_eq!(2, find_answer(16));
+        assert_eq!(17, find_answer(17));
+
+        assert_eq!(29, find_answer(13195));
+        println!("{}", find_answer(600851475143));
     }
 }
