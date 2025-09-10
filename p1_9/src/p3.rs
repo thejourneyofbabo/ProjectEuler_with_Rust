0cd64a9514830c3735f8134c992ed83820658d70
Jisang Yun <journeyofbabo@gmail.com>
2025-09-11 06:23:44 +0900

    attempt2

--- p1_9/src/p3.rs
+++ p1_9/src/p3.rs
@@ -4,6 +4,7 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143?
 */
 
+#[allow(dead_code)]
 #[cfg(test)]
 fn find_answer(mut n: u64) -> u64 {
     let mut p = 2; // 2 smallest prime number
@@ -27,17 +28,33 @@ fn find_answer(mut n: u64) -> u64 {
 }
 
 #[cfg(test)]
+fn find_answer1(mut n: u64) -> u64 {
+    let mut p = 2;
+    while n > 1 {
+        if 0 == n % p {
+            n /= p;
+        } else {
+            p += 1;
+        }
+    }
+    if n != 1 {
+        p = n;
+    }
+    p
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test() {
         println!("test_template");
-        assert_eq!(5, find_answer(10));
-        assert_eq!(2, find_answer(16));
-        assert_eq!(17, find_answer(17));
+        assert_eq!(5, find_answer1(10));
+        assert_eq!(2, find_answer1(16));
+        assert_eq!(17, find_answer1(17));
 
-        assert_eq!(29, find_answer(13195));
-        println!("{}", find_answer(600851475143));
+        assert_eq!(29, find_answer1(13195));
+        println!("{}", find_answer1(600851475143));
     }
 }
