04627e334e102bda85a908f1be49100fc8e80f6f
Jisang Yun <journeyofbabo@gmail.com>
2025-09-12 09:19:38 +0900

    method 3, skip 2steps

--- p1_9/src/p3.rs
+++ p1_9/src/p3.rs
@@ -44,6 +44,7 @@ fn find_answer1(mut n: u64) -> u64 {
     p
 }
 
+#[allow(dead_code)]
 #[cfg(test)]
 fn find_answer2(mut n: u64) -> u64 {
     let mut p = 2;
@@ -66,17 +67,49 @@ fn find_answer2(mut n: u64) -> u64 {
 }
 
 #[cfg(test)]
+fn find_answer3(mut n: u64) -> u64 {
+    // Check about 2
+    let mut last_factor = 2;
+    if n % 2 == 0 {
+        n /= 2;
+        while n % 2 == 0 {
+            n /= 2;
+        }
+    } else {
+        last_factor = 1;
+    }
+
+    // Prime number over 2
+    let mut p = 3;
+    let mut max_factor = (n as f64).sqrt() as u64;
+    while n > 1 && p <= max_factor {
+        if 0 == n % p {
+            n /= p;
+            last_factor = p;
+            max_factor = (n as f64).sqrt() as u64;
+        } else {
+            p += 2; // Can check every 2 steps
+        }
+    }
+    if n != 1 {
+        last_factor = n;
+    }
+
+    last_factor
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test() {
         println!("test_template");
-        assert_eq!(5, find_answer2(10));
-        assert_eq!(2, find_answer2(16));
-        assert_eq!(17, find_answer2(17));
+        assert_eq!(2, find_answer3(16));
+        assert_eq!(5, find_answer3(10));
+        assert_eq!(17, find_answer3(17));
 
-        assert_eq!(29, find_answer2(13195));
-        println!("{}", find_answer2(600851475143));
+        assert_eq!(29, find_answer3(13195));
+        println!("{}", find_answer3(600851475143));
     }
 }
