1ec531fbbe235af9995dca100daf3c92c43d3340
Jisang Yun <journeyofbabo@gmail.com>
2025-09-13 12:15:00 +0900

    upgrade

--- p1_9/src/p4.rs
+++ p1_9/src/p4.rs
@@ -5,6 +5,7 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
+#[allow(dead_code)]
 #[cfg(test)]
 fn is_palindrome(num: u32) -> bool {
     let mut n = num;
@@ -17,6 +18,7 @@ fn is_palindrome(num: u32) -> bool {
     num == rev
 }
 
+#[allow(dead_code)]
 #[cfg(test)]
 fn find_palindrome() -> u32 {
     // 1. find from [999..=100]
@@ -41,11 +43,55 @@ fn find_palindrome() -> u32 {
 }
 
 #[cfg(test)]
+// check the palindrome number of n is the product of two integer of not
+fn is_product(n: u32) -> bool {
+    let mut ans: bool = false;
+    for j in (100..=999).rev() {
+        let (q, r): (u32, u32) = (n / j, n % j);
+        if r == 0 && q <= 999 {
+            ans = true;
+            break;
+        }
+        if q > 999 {
+            break;
+        }
+    }
+    ans
+}
+
+#[cfg(test)]
+fn rev_num(mut n: u32) -> u32 {
+    let mut rev: u32 = 0;
+    while n > 0 {
+        rev = 10 * rev + n % 10;
+        n /= 10;
+    }
+    rev
+}
+
+#[cfg(test)]
+fn find_palindrome3() -> u32 {
+    // 1. make plaindromic num
+    let mut x: u32 = 999;
+    let mut max: u32 = 1;
+    while x >= 100 {
+        let y = rev_num(x);
+        let p = x * 1000 + y;
+        if is_product(p) {
+            max = p;
+            break;
+        }
+        x -= 1;
+    }
+    max
+}
+
+#[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test() {
-        println!("{}", find_palindrome());
+        println!("{}", find_palindrome3());
     }
 }
