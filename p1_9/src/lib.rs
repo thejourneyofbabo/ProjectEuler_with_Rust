ca605495a6f641deda8dafb121d6d94a7a2e07cb
Jisang Yun <journeyofbabo@gmail.com>
2025-09-17 08:52:56 +0900

    p6 base

--- p1_9/src/lib.rs
+++ p1_9/src/lib.rs
@@ -3,6 +3,7 @@ mod p2;
 mod p3;
 mod p4;
 mod p5;
+mod p6;
 mod test_template;
 
 pub fn add(left: u64, right: u64) -> u64 {
---
+++ p1_9/src/p6.rs
@@ -0,0 +1,28 @@
+// p6.rs
+/*
+The sum of the squares of the first ten natural numbers is,
+1^2 + 2^2 + ... 10^2 = 385
+
+The sauare of the sum of the first ten natural numbers is,
+(1 + 2 + ... 10)^2 = 3025
+
+Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 - 385 = 2640
+
+Find the difference between the sum of squares of the first one hundred natural numbers and the square of the sum.
+*/
+
+#[cfg(test)]
+fn base() {
+    println!("base");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        println!("test_template");
+        base();
+    }
+}
