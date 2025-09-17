2a13b6244a00c970d6aece923e80045fd892c681
Jisang Yun <journeyofbabo@gmail.com>
2025-09-17 09:10:13 +0900

    p6 solve

--- p1_9/src/p6.rs
+++ p1_9/src/p6.rs
@@ -12,8 +12,36 @@ Find the difference between the sum of squares of the first one hundred natural 
 */
 
 #[cfg(test)]
-fn base() {
-    println!("base");
+fn get_seq_sum(n: u64) -> u64 {
+    n * (n + 1) / 2
+}
+
+#[allow(dead_code)]
+#[cfg(test)]
+fn get_square_sum(n: u64) -> u64 {
+    let mut sum: u64 = 0;
+    for i in 1..=n {
+        sum += i * i;
+    }
+    sum
+}
+
+#[cfg(test)]
+fn get_square_sum1(n: u64) -> u64 {
+    n * (n + 1) * (2 * n + 1) / 6
+}
+
+#[cfg(test)]
+fn answer(n: u64) -> u64 {
+    // 1. a = (1 + .. + 100)^2
+    let mut a: u64 = get_seq_sum(n);
+    a *= a;
+
+    // 2. b = (1^2 + ... 100^2)
+    let b: u64 = get_square_sum1(n);
+
+    // 3. a-b
+    a - b
 }
 
 #[cfg(test)]
@@ -22,7 +50,10 @@ mod tests {
 
     #[test]
     fn test() {
-        println!("test_template");
-        base();
+        assert_eq!(0, answer(1));
+        assert_eq!(4, answer(2));
+        assert_eq!(22, answer(3));
+        assert_eq!(2640, answer(10));
+        println!("{}", answer(100));
     }
 }
