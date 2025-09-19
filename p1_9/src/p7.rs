e9db8ba3b03235f29eb560ca3d7d3a2c5deeef8c
Jisang Yun <journeyofbabo@gmail.com>
2025-09-20 03:29:11 +0900

    initial submit p7

--- p1_9/src/p7.rs
+++ p1_9/src/p7.rs
@@ -4,8 +4,24 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11 and 13, we can see that t
 */
 
 #[cfg(test)]
-fn base() {
-    println!("base");
+fn answer(n: u64) -> u64 {
+    let mut ans: u64 = 2;
+    let mut cnt: u64 = 1;
+    while cnt < n {
+        ans += 1;
+        let kmax: u64 = (ans as f64).sqrt() as u64;
+        let mut flag = true;
+        for k in 2..=kmax {
+            if ans % k == 0 {
+                flag = false;
+                break;
+            }
+        }
+        if flag {
+            cnt += 1;
+        }
+    }
+    ans
 }
 
 #[cfg(test)]
@@ -14,7 +30,10 @@ mod tests {
 
     #[test]
     fn test() {
-        println!("test_template");
-        base();
+        assert_eq!(2, answer(1));
+        assert_eq!(3, answer(2));
+        assert_eq!(5, answer(3));
+        assert_eq!(13, answer(6));
+        println!("{}", answer(10001));
     }
 }
